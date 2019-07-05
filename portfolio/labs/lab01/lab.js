3b6a5a641cd3a95ccb7a24351ae329d9c680adbf
xualison96 <52521311+xualison96@users.noreply.github.com>
2019-07-05 17:57:38 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,6 +4,39 @@
 
 
 function start(e) {
+    var name = [
+        {
+            char:'許',
+            big5:'B35C',
+            cns:'1-5D42',
+            unicode:'8A31'
+        },
+        {
+            char:'崇',
+            big5:'B152',
+            cns:'1-5976',
+            unicode:'5D07'
+        },
+        {
+            char:'祐',
+            big5:'AFA7',
+            cns:'1-572A',
+            unicode:'7950'
+        }
+    ];
+
+    var ele =document.getElementById('div1');
+
+    for (var i=0 ; i<3 ; i++ ) {
+        ele.innerHTML += name[i].char;
+        ele.innerHTML += '    big5=';
+        ele.innerHTML += name[i].big5;
+        ele.innerHTML += '    cns=';
+        ele.innerHTML += name[i].cns;
+        ele.innerHTML += '    unicode=';
+        ele.innerHTML += name[i].unicode;
+        ele.innerHTML += '<br>';
+    }
 
 }
 
--- portfolio/labs/lab02/lab.js
+++ portfolio/labs/lab02/lab.js
@@ -3,7 +3,28 @@
 */
 
 
+function update() {
+    
+    var x = document.getElementById('in1').value;
+    var ele = document.getElementById('info1');
+    var y = '';
+
+    for (var i = 0 ; i<x.length ; i++){
+        y += x.charAt(i)
+        y += ' U+ ' ;
+        y += x.charCodeAt(i).toString(16).toUpperCase();
+        y += '<br>';
+    }
+
+    ele.innerHTML = y;
+    
+    document.getElementById('info3').innerHTML=
+    document.getElementById('in1').value
+}
+
 function start(e) {
+    document.getElementById('in1').addEventListener("input", update);
+    update();
 
 }
 
